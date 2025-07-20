9604e6e32608d2475cb59482cd2569c43496ff0a
SyedRehanAli25 <rehan.ali9325@gmail.com>
2025-07-20 16:41:42 +0530

    Add global function and class to shared library

---
+++ src/org/devopstitans/Utils.groovy
@@ -0,0 +1,8 @@
+package org.devopstitans
+
+class Utils {
+    static String shout(String message) {
+        return message.toUpperCase() + "!!!"
+    }
+}
+
---
+++ vars/sayHello.groovy
@@ -0,0 +1,4 @@
+def call(String name = 'DevOps Ninja') {
+    echo "Hello, ${name} from Shared Library!"
+}
+
