1f65b0dd312a78449689c5de79c67f5445e81445
SyedRehanAli25 <rehan.ali9325@gmail.com>
2025-07-25 22:40:41 +0530

    Update k8DeployPipeline.groovy

--- vars/k8DeployPipeline.groovy
+++ vars/k8DeployPipeline.groovy
@@ -12,7 +12,7 @@ def call(Map params = [:]) {
                     script {
                         def envName = params.ENVIRONMENT ?: params.env ?: ENVIRONMENT
                         if (!envName) {
-                            error "❌ ENVIRONMENT not provided. Please pass it as a parameter or call like k8DeployPipeline(env: 'dev')"
+                            error " ENVIRONMENT not provided. Please pass it as a parameter or call like k8DeployPipeline(env: 'dev')"
                         }
 
                         def configPath = "resources/configs/${envName}-config.yaml"
@@ -27,7 +27,7 @@ def call(Map params = [:]) {
                         env.KEEP_APPROVAL_STAGE   = config.KEEP_APPROVAL_STAGE.toString()
                         env.DEPLOY_ENV            = config.ENVIRONMENT
 
-                        notifySlack("🚀 ${config.ACTION_MESSAGE}", config.SLACK_WEBHOOK_URL)
+                        notifySlack(" ${config.ACTION_MESSAGE}", config.SLACK_WEBHOOK_URL)
                     }
                 }
             }
@@ -37,14 +37,14 @@ def call(Map params = [:]) {
                     expression { return env.KEEP_APPROVAL_STAGE == 'true' }
                 }
                 steps {
-                    input message: "🟢 Approve deployment to ${env.DEPLOY_ENV}?"
+                    input message: " Approve deployment to ${env.DEPLOY_ENV}?"
                 }
             }
 
             stage('Deploy to Kubernetes') {
                 steps {
                     script {
-                        echo "🚀 Deploying manifests from ${env.CODE_BASE_PATH} to ${env.DEPLOY_ENV} environment"
+                        echo " Deploying manifests from ${env.CODE_BASE_PATH} to ${env.DEPLOY_ENV} environment"
                         sh """
                         kubectl apply -f ${env.CODE_BASE_PATH}/ --namespace=${env.DEPLOY_ENV}
                         kubectl rollout status deployment/my-app -n ${env.DEPLOY_ENV}
@@ -57,12 +57,12 @@ def call(Map params = [:]) {
         post {
             success {
                 script {
-                    notifySlack("✅ Deployment to *${env.DEPLOY_ENV}* succeeded", env.SLACK_WEBHOOK_URL)
+                    notifySlack(" Deployment to *${env.DEPLOY_ENV}* succeeded", env.SLACK_WEBHOOK_URL)
                 }
             }
             failure {
                 script {
-                    notifySlack("❌ Deployment to *${env.DEPLOY_ENV}* failed", env.SLACK_WEBHOOK_URL)
+                    notifySlack(" Deployment to *${env.DEPLOY_ENV}* failed", env.SLACK_WEBHOOK_URL)
                 }
             }
         }
